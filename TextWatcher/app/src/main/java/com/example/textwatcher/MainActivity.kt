54ee0b65a80b2c508495a575af55f1e3851e7cf5
riyapatel8899 <61073723+riyapatel8899@users.noreply.github.com>
2025-06-04 15:53:41 +0530

    Added TextWatcher

---
+++ TextWatcher/app/src/main/java/com/example/textwatcher/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.textwatcher
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextName: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var loginButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        editTextName = findViewById(R.id.editTextName)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        loginButton = findViewById(R.id.login_button)
+
+        editTextName.addTextChangedListener(textWatcher)
+        editTextPassword.addTextChangedListener(textWatcher)
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            loginButton.isEnabled = !editTextName.text.toString().trim().isEmpty()
+                    && !editTextPassword.text.toString().trim().isEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
+    }
+}
